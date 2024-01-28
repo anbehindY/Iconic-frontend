e38b05e60077d4055778a82d0f38a75ca55fe795
sawthetphyoe <sawthetphyoe28498@gmail.com>
2024-01-29 00:00:46 +0630

    Slider - update style

--- app/components/ImageSlider.tsx
+++ app/components/ImageSlider.tsx
@@ -20,27 +20,27 @@ export default function ImageSlider() {
       small: string;
       thumb: string;
     };
-  }) => {
+  }) : React.ReactNode => {
     // return (
     //   <div
     //     style={{ backgroundImage: `url(${item.urls.full})`, width: "100vw" }}
     //     className="bg-no-repeat bg-center bg-cover h-[calc(100vh-76px)]"
     //   />
     // );
-
+    //
     const GalleriaItem = () => {
       return (
         <Image
           src={item.urls.full}
           alt={"Image"}
-          fill
-          objectFit={"cover"}
-          className={"animate-fade-in"}
+          width={1920}
+          height={1080}
+          className={"animate-fade-in w-full"}
         />
       );
     };
     return (
-      <div className={"h-[calc(100vh-76px)] w-full relative"}>
+      <div className={"!h-[calc(100vh-76px)] !w-full"}>
         <GalleriaItem />
       </div>
     );
@@ -57,11 +57,11 @@ export default function ImageSlider() {
       item={itemTemplate}
       autoPlay
       transitionInterval={5000}
-      className={"overflow-hidden"}
-      // pt={{
-      //   nextItemButton: { className: "rounded-[2em]" },
-      //   previousItemButton: { className: "rounded-[2em]" },
-      // }}
+      className={"overflow-y-hidden"}
+      pt={{
+        nextItemButton: { className: "rounded-[2em]" },
+        previousItemButton: { className: "rounded-[2em]" },
+      }}
     />
   );
 }
