c96d08bcd9bd567117fa779c6847c86b88fc7fcd
anbehindY <yanminthwin@gmail.com>
2024-03-26 14:35:32 +0700

    Add collection cards

---
+++ app/components/mainPage/Cards.tsx
@@ -0,0 +1,8 @@
+export default function Cards() {
+  return (
+    <section className="flex justify-center items-center gap-8 px-20 py-20 w-full h-full">
+      <div className="w-[600px] h-[650px] bg-slate-300/20"> Hello</div>
+      <div className="w-[600px] h-[650px] bg-red-300"> Hello</div>
+    </section>
+  );
+}
