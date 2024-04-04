d8ca054e4577b96390d7216d85bea935de6ae6ed
anbehindY <yanminthwin@gmail.com>
2024-04-05 02:01:28 +0700

    Update collection layout

--- components/mainPage/Collections.tsx
+++ components/mainPage/Collections.tsx
@@ -1,10 +1,32 @@
 import Collection from "./Collection";
 
 export default function Collections() {
+  const COLLECTIONS = [
+    {
+      name: 'iPhone',
+      img: 'iphone',
+    },
+    {
+      name: 'Macbook',
+      img: 'mac',
+    },
+    {
+      name: 'iPad',
+      img: 'ipad',
+    },
+    {
+      name: 'Apple Watch',
+      img: 'apple_watch',
+    },
+    {
+      name: 'AirPods',
+      img: 'airpods',
+    }];
+
   return (
     <section className="bg-slate-300/20 flex justify-center items-center gap-16 px-4 py-32 w-full">
-      {[1, 2, 3, 4, 5].map((item) => {
-        return <Collection key={item} item={item} />;
+      {COLLECTIONS.map((item) => {
+        return <Collection key={item.name} img={item.img} name={item.name}/>;
       })}
     </section>
   );
