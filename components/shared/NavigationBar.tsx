2803bc6112dc40d60829f3d098107ece312dc1bd
anbehindY <yanminthwin@gmail.com>
2024-04-05 14:37:46 +0700

    Change nav bar padding

--- components/shared/NavigationBar.tsx
+++ components/shared/NavigationBar.tsx
@@ -10,7 +10,7 @@ export default function NavigationBar() {
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
         {/* Navbar */}
-        <div className="w-full navbar bg-white px-16 py-3">
+        <div className="w-full navbar bg-white px-16 py-[18px]">
           <div className="flex-none lg:hidden">
             <label
               htmlFor="my-drawer-3"
