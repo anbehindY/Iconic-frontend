6f426b0c86ed1cdc4b9e16a3968509146274f00b
anbehindY <yanminthwin@gmail.com>
2024-04-08 03:23:42 +0700

    Add collection detail page

---
+++ app/collections/[id]/_components/ColorPicker.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import clsx from "clsx";
+import Image from "next/image";
+
+type ColorPickerProps = {
+  item: number;
+  active: number;
+  onClickHanlder: any;
+};
+
+export default function ColorPicker({ item, active, onClickHanlder }: ColorPickerProps) {
+  return (
+    <div
+      className={clsx(
+        "w-12 h-12 p-[2px] rounded-lg relative aspect-square border-2",
+        {
+          "border-black": active == item,
+          "border-transparent": active != item,
+        }
+      )}
+      onClick={() => onClickHanlder(item)}
+    >
+      <Image src="/dummy_iphone.webp" alt="demo" fill className="rounded-lg" />
+    </div>
+  );
+}
